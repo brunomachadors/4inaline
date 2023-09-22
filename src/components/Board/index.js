e975f0d6249b697550419f21506fa0b8449d5c2f
Tatiana Taketsuma <tatiana.taketsuma@mindera.com>
2023-09-22 21:53:47 +0100

    changes on board file

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
-import Square from '../Square';
+import React, { useState, Fragment } from "react";
+import Square from "../Square";
 
 const Board = (props) => {
   const [squares, setSquares] = useState(Array(42).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const { numberOfColumns, numberOfLines } = props;
-  const gameMatrix = new Array(numberOfLines).fill(
-    new Array(numberOfColumns).fill(undefined)
+  const gameMatrix = new Array(numberOfColumns).fill(
+    new Array(numberOfLines).fill(undefined)
   );
+  const buttonArray = new Array(7);
 
   const handleClick = (i) => {
     console.log(i);
     const newSquares = squares.slice();
 
     if (xIsNext) {
-      newSquares[i] = 'X';
+      newSquares[i] = "X";
     } else {
-      newSquares[i] = 'O';
+      newSquares[i] = "O";
     }
 
     setSquares(newSquares);
@@ -24,22 +25,25 @@ const Board = (props) => {
   };
 
   return (
-    <>
-      {gameMatrix.map((line) => (
-        <div style={{ display: 'flex' }}>
-          {line.map((column) => (
-            <div
-              style={{
-                margin: '4px',
-                width: '20px',
-                height: '20px',
-                backgroundColor: 'green',
-              }}
-            ></div>
-          ))}
+    <div style={{ display: 'flex' }}>
+      {gameMatrix.map((column, index) => (
+        <div key={`column-${index}`} style={{ display: "flex", flexDirection: 'column' }}>
+          {column.map((line, index) => (
+                <div
+                  key={`line-${index}`}
+                  style={{
+                    margin: "4px",
+                    width: "20px",
+                    height: "20px",
+                    backgroundColor: "green",
+                  }}
+                ></div>
+            )
+          )}
+          <button type="button">{index + 1}</button>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
