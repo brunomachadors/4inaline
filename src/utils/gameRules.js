3e606e77b6d6babacbf52769871670cae298ad63
brunomachadors <brunomachadors@gmail.com>
2023-09-23 22:21:11 +0100

    Removing game rules

--- src/utils/gameRules.js
+++
@@ -1,8 +0,0 @@
-export function insertCoin(column, player) {
-  for (let i = column.length - 1; i >= 0; i--) {
-    if (column[i] === null) {
-      column[i] = player;
-      return column;
-    }
-  }
-}
