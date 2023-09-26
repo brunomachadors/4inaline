48a34ad2b75905dd835868bfcd143788964126bb
brunomachadors <brunomachadors@gmail.com>
2023-09-26 11:20:44 +0100

    Fix rules

--- src/components/Game/style.js
+++ src/components/Game/style.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export const GameUi = styled.body`
   font: 16px 'Century Gothic', Futura, sans-serif;
   margin: 20px;
+  display: flex;
+  flex-direction: column;
+  background-color: #e6f3fb;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Title = styled.h1`
@@ -15,4 +20,6 @@ export const Rules = styled.footer`
   border: solid 1px black;
   display: flex;
   justify-content: center;
+  align-items: center;
+  width: 55%;
 `;
