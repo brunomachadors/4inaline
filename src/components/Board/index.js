868302d4e7f4017a98974ab8c938d10631b8d0b2
Alda Oliveira <aldaoliveira35@gmail.com>
2023-09-30 12:08:08 +0100

    cleans up board and game components

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,80 +1,49 @@
-import React, { useState } from 'react';
-import { checkWin } from '../../utils/checkWin';
-import { createBoard } from '../../utils/createBoard';
-import { RowUi, ColumnUi, BoardUi } from './style';
+import { RowUi, ColumnUi, BoardUi } from "./style";
 import {
   ButtonImageUi,
   ButtonUi,
   ButtonsContainer,
   PlayAgainButton,
-} from '../Buttons/style';
+} from "../Buttons/style";
 import {
   Player1ColorCircle,
   Player2ColorCircle,
   PlayerColor,
   PlayerContainer,
   WinnerTitle,
-} from '../Player/style';
-import { Rules, Title } from '../Game/style';
-import { PLAYER_COLORS } from '../../utils/playerColors';
+} from "../Player/style";
+import { Rules, Title } from "../Game/style";
+import { PLAYER_COLORS } from "../../utils/playerColors";
 
-const debugMode = false;
-
-export function Board({ numberOfColumns, numberOfRows }) {
-  const [winner, setWinner] = useState(null);
-  const [player, setPlayer] = useState('player1');
+export function Board({
+  numberOfColumns,
+  onPlay,
+  winner,
+  onStartGame,
+  boardState,
+  draw,
+}) {
+  const debugMode = false;
   const buttons = [];
-
-  const [boardState, setBoardState] = useState(() => {
-    return createBoard(numberOfColumns, numberOfRows);
-  });
 
   for (let column = 0; column < numberOfColumns; column++) {
     buttons.push(
       <ButtonUi
         key={column}
         disabled={winner !== null}
-        onClick={() => {
-          let updatedBoardState = structuredClone(boardState);
-          for (let row = numberOfRows - 1; row >= 0; row--) {
-            if (updatedBoardState[row][column] === null) {
-              updatedBoardState[row][column] = player;
-              break;
-            }
-          }
-
-          setBoardState(updatedBoardState);
-          setPlayer(player === 'player1' ? 'player2' : 'player1');
-          if (checkWin(updatedBoardState, player)) {
-            setWinner(player);
-          }
-        }}
+        onClick={() => onPlay(column)}
       >
         <ButtonImageUi src="https://cdn-icons-png.flaticon.com/512/55/55008.png"></ButtonImageUi>
       </ButtonUi>
     );
   }
 
-  const handlePlayAgainClick = () => {
-    startGame();
-  };
-
-  const startGame = () => {
-    setPlayer('player1');
-    setWinner(null);
-    setBoardState(createBoard(numberOfColumns, numberOfRows));
-  };
-
-  const draw =
-    !winner &&
-    boardState.every((row) => row.every((column) => column !== null));
-
   return (
     <>
       <Title>4 in a line</Title>
 
       <Rules>
-        The four in a row rules are very simple. It’s always played with 2
+        The four in a row rules are very simple. It's always played with 2
         players and in a 7x6 grid. Each turn each player puts a piece of his
         color inside a column and it will fall until it reaches the lowest
         available spot. The one who can put 4 pieces of the same color in a row
@@ -82,9 +51,7 @@ export function Board({ numberOfColumns, numberOfRows }) {
         then the match ends in a draw.
       </Rules>
 
-      <PlayAgainButton onClick={handlePlayAgainClick}>
-        Play again
-      </PlayAgainButton>
+      <PlayAgainButton onClick={onStartGame}>Play again</PlayAgainButton>
 
       {boardState.map((row, rowIndex) => (
         <BoardUi>
@@ -95,7 +62,7 @@ export function Board({ numberOfColumns, numberOfRows }) {
                   backgroundColor: PLAYER_COLORS[column],
                 }}
               >
-                {debugMode ? rowIndex + ' - ' + columnIndex : null}
+                {debugMode ? rowIndex + " - " + columnIndex : null}
               </ColumnUi>
             ))}
           </RowUi>
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,10 +1,55 @@
-import { Board } from '../Board';
-import { GameUi } from './style';
+import { useState } from "react";
+import { Board } from "../Board";
+import { GameUi } from "./style";
+import { createBoard } from "../../utils/createBoard";
+import { checkWin } from "../../utils/checkWin";
 
 const Game = () => {
+  const numberOfColumns = 7;
+  const numberOfRows = 6;
+  const [player, setPlayer] = useState("player1");
+  const [winner, setWinner] = useState(null);
+  const [boardState, setBoardState] = useState(() => {
+    return createBoard(numberOfColumns, numberOfRows);
+  });
+
+  const startGame = () => {
+    setPlayer("player1");
+    setWinner(null);
+    setBoardState(createBoard(numberOfColumns, numberOfRows));
+  };
+
+  const handlePlay = (column) => {
+    let updatedBoardState = structuredClone(boardState);
+    for (let row = numberOfRows - 1; row >= 0; row--) {
+      if (updatedBoardState[row][column] === null) {
+        updatedBoardState[row][column] = player;
+        break;
+      }
+    }
+
+    setBoardState(updatedBoardState);
+    setPlayer(player === "player1" ? "player2" : "player1");
+    if (checkWin(updatedBoardState, player)) {
+      setWinner(player);
+    }
+  };
+
+  const draw =
+    !winner &&
+    boardState.every((row) => row.every((column) => column !== null));
+
   return (
     <GameUi>
-      <Board numberOfColumns={7} numberOfRows={6} />
+      <Board
+        numberOfColumns={7}
+        numberOfRows={6}
+        onPlay={handlePlay}
+        winner={winner}
+        onStartGame={startGame}
+        boardState={boardState}
+        draw={draw}
+      />
     </GameUi>
   );
 };
